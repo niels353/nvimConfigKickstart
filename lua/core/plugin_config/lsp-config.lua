5c66a73059de78588e2cef9cdb397746d470c693
pahlnick <Niels.Pahl@ruhr-uni-bochum.de>
2024-05-25 12:03:56 +0200

    added completions

--- lua/core/plugin_config/lsp-config.lua
+++ lua/core/plugin_config/lsp-config.lua
@@ -4,9 +4,13 @@ require("mason-lspconfig").setup({
    ensure_installed = { "lua_ls" }
 })
 
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 local lspconfig = require('lspconfig')
 
-lspconfig.lua_ls.setup({})
+lspconfig.lua_ls.setup({
+   capabilities = capabilities
+})
 
 vim.api.nvim_create_autocmd('LspAttach', {
    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,26 +1,33 @@
 require("lazy").setup({
-   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-   },
-   {
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v3.x",
-      dependencies = {
-         "nvim-lua/plenary.nvim",
-         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-         "MunifTanjim/nui.nvim",
-      }
-   },
-   {
-      'nvim-lualine/lualine.nvim',
-      dependencies = { 'nvim-tree/nvim-web-devicons' }
-   },
-   "nvim-treesitter/nvim-treesitter",
-   "williamboman/mason.nvim",
-   "williamboman/mason-lspconfig.nvim",
-   "neovim/nvim-lspconfig",
-   "nvim-telescope/telescope-ui-select.nvim",
-   "nvimtools/none-ls.nvim"
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	"nvim-treesitter/nvim-treesitter",
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	"nvim-telescope/telescope-ui-select.nvim",
+	"nvimtools/none-ls.nvim",
+	-- Completions
+	"hrsh7th/nvim-cmp",
+	"L3MON4D3/LuaSnip",
+	"saadparwaiz1/cmp_luasnip",
+	"rafamadriz/friendly-snippets",
+   "hrsh7th/cmp-nvim-lsp",
 })
