14acb19022ca3a9f7ef1042705841cc9b0818a48
niels353 <pahlniels525@gmail.com>
2024-06-04 15:06:08 +0200

    added completion keybinds

--- lua/core/plugin_config/completions.lua
+++ lua/core/plugin_config/completions.lua
@@ -1,28 +1,39 @@
 local cmp = require("cmp")
 require("luasnip.loaders.from_vscode").lazy_load()
+local luasnip = require("luasnip")
 
 cmp.setup({
-	snippet = {
-		-- REQUIRED - you must specify a snippet engine
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
-		end,
-	},
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	}),
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" }, -- For luasnip users.
-	}, {
-		{ name = "buffer" },
-	}),
+   snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+         require("luasnip").lsp_expand(args.body)
+      end,
+   },
+   window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+   },
+   mapping = cmp.mapping.preset.insert({
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ['<C-l>'] = cmp.mapping(function()
+         if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+         end
+      end, { 'i', 's' }),
+      ['<C-h>'] = cmp.mapping(function()
+         if luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+         end
+      end, { 'i', 's' }),
+   }),
+   sources = cmp.config.sources({
+      { name = "nvim_lsp" },
+      { name = "luasnip" }, -- For luasnip users.
+   }, {
+      { name = "buffer" },
+   }),
 })
